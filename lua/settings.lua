8c1efd01b971c708a56d9fdae1d9b7baac31146e
Варламов Николай Леонидович <nikolay.varlamov@sbermegamarket.ru>
2022-05-28 00:37:20 +0300

    [master] added settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,27 +1,41 @@
 local cmd = vim.cmd
-local exec = vim.api.nvim_exec
-local g = vim.g   
-local opt = vim.opt   
+local fn = vim.fn
 
--- Main
-opt.colorcolumn = '120' 
-opt.number = true
-opt.relativenumber = true
+-- Main Vim settings
+vim.cmd('set colorcolumn=120')  	-- colorcolumn
+vim.cmd('set relativenumber')       -- relative numbers
+vim.cmd('set encoding=utf8')		-- encoding
+vim.cmd('set nu!')			        -- line numbers 
+vim.cmd('set mouse=a')          	-- enabling mouse
+vim.cmd('set wildmenu')         	-- commandline completion
+vim.cmd('set incsearch')        	-- incsearch
+vim.cmd('set title')            	-- tab title
+vim.cmd('set shiftwidth=4')     	-- tab width
+vim.cmd('set softtabstop=4')    	-- spaces on tab
+vim.cmd('set tabstop=4')        	-- spaces on tab
+vim.cmd('set smarttab')         	-- shifwidth
+vim.cmd('set expandtab')        	-- indents on > and <
+vim.cmd('set smartindent')      	-- smart indents
 
--- Tabulation
+-- Tabulation settings
 cmd([[
     filetype indent plugin on
     syntax enable
 ]])
-
-cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
-
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.smartindent = true
+cmd('au BufEnter * set fo-=c fo-=r fo-=o')
 
 -- Theme
-cmd [[colorscheme nord]]
+cmd('colorscheme nord')
+
+-- Packer autoinstall 
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({
+		'git', 'clone', '--depth', '1', 
+		'https://github.com/wbthomason/packer.nvim', 
+		install_path
+	})
+end
 
 -- LSP settings
 local lsp_installer = require("nvim-lsp-installer")

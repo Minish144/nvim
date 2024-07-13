5df346271df15cf8f35e3ec8ffca06f6a73d90ce
Варламов Николай Леонидович <nikolay.varlamov@sbermegamarket.ru>
2024-07-14 00:14:25 +0300

    Configure smartcolumn

--- lua/plugins/smartcolumn.lua
+++ lua/plugins/smartcolumn.lua
@@ -3,6 +3,20 @@ return {
     "m4xshen/smartcolumn.nvim",
     opts = {
       custom_colorcolumn = "135",
+      disabled_filetypes = {
+        "defx",
+        "netrw",
+        "NvimTree",
+        "lazy",
+        "mason",
+        "help",
+        "checkhealth",
+        "lspinfo",
+        "noice",
+        "Trouble",
+        "fish",
+        "zsh",
+      },
     },
   },
 }

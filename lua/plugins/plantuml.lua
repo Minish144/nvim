f2ffe9a8b922dd3ea32a5e126f858d4a92f1bb7b
Варламов Николай Леонидович <varlamow.col@yahoo.com>
2024-09-25 21:08:02 +0300

    Add plantuml

---
+++ lua/plugins/plantuml.lua
@@ -0,0 +1,13 @@
+return {
+
+  {
+    "weirongxu/plantuml-previewer.vim",
+    ft = { "plantuml" },
+    dependencies = { { "tyru/open-browser.vim" } },
+    wants = { "open-browser.vim" },
+  },
+  {
+    "aklt/plantuml-syntax",
+    ft = { "plantuml" },
+  },
+}

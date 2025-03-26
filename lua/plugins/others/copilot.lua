711bfb7f786f6fece6aca28f978f20048e2014b8
Nikolai Varlamov <varlamow.col@yahoo.com>
2025-03-26 23:17:22 +0300

    Add github copilot plugin

---
+++ lua/plugins/others/copilot.lua
@@ -0,0 +1,19 @@
+return {
+	"github/copilot.vim",
+	config = function()
+		local env_path = vim.fn.stdpath("config") .. "/.env"
+		local file = io.open(env_path, "r")
+		if file then
+			for line in file:lines() do
+				local key, value = line:match("([^=]+)=([^\r\n]+)")
+				if key and value and key == "PROXY" then
+					vim.g.copilot_proxy = value
+				end
+			end
+			file:close()
+		end
+
+		-- Можно включить Copilot сразу
+		vim.cmd("Copilot enable")
+	end,
+}
